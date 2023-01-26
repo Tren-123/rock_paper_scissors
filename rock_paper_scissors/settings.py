7d517948a098327ed2f60d2774b504a0a8c871a5
Tren-123 <pavel.filatov1@yandex.ru>
2023-01-26 15:19:29 +0300

    Chanched config for deploy

--- rock_paper_scissors/settings.py
+++ rock_paper_scissors/settings.py
@@ -93,7 +93,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG' : {
-            'hosts': [(os.environ.get('REDIS_URL'), 6379)]
+            'hosts': [(os.environ.get('REDIS_HOST'), (os.environ.get('REDIS_PORT')))]
         }
     }
 }
