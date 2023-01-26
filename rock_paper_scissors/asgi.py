7921fad07aab47011b41423625afb007e6a62bed
Tren-123 <pavel.filatov1@yandex.ru>
2023-01-26 20:19:09 +0300

    changed asgi.py and config for deploying

--- rock_paper_scissors/asgi.py
+++ rock_paper_scissors/asgi.py
@@ -14,13 +14,11 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-import django
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rock_paper_scissors.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-django.setup()
 django_asgi_app = get_asgi_application()
 
 from game.routing import ws_urlpatterns
