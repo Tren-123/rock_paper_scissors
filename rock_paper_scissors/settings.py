0b52590861cecc35cb02b8558b1b8f37b5922447
Tren-123 <pavel.filatov1@yandex.ru>
2023-01-27 11:23:04 +0300

    Added csrf trusted origins in settings

--- rock_paper_scissors/settings.py
+++ rock_paper_scissors/settings.py
@@ -32,6 +32,10 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://rock-paper-scissors-bl9y.onrender.com'
+]
+
 # Application definition
 
 INSTALLED_APPS = [
